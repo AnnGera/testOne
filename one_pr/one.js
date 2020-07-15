8485016a1ad876c96211e5755e9d3a62b4887bc9
Ann <anngeran@gmail.com>
2020-07-15 18:15:51 +0300

    commit

--- one_pr/one.js
+++ one_pr/one.js
@@ -1,7 +1,7 @@
 var str="privet mir";
  str=str[0].toUpperCase()+str.substring(1)
 console.log(str)
-
+//второй способ
 function BigFirst(a){
     return a.charAt(0).toUpperCase()+a.substring(1);
 }
