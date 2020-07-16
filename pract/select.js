1414c4b1e9fcc7b1750b7ac7ca438ae17b792d58
Ann <anngeran@gmail.com>
2020-07-16 17:52:14 +0300

    New

--- pract/select.js
+++ pract/select.js
@@ -1,12 +1,10 @@
-<script>option(['01','05','10','15','20','25'])</script>
-
-function option(massive) {
-let sys1 = '<option value="0.'
-let sys2 = '">'
-let sys3 = '</option>'
+function option(arr) {
+var a = '<option value="0.',
+ b = '">',
+ c = '</option>';
 let result = "";
 for (var i = 0; i < 6; i++) {
-result+=sys1+massive[i]+sys2+Number(massive[i])+sys3;
+result+=a+arr[i]+b+Number(arr[i])+c;
 }
 document.getElementById("service_quality").innerHTML = result;
 }
