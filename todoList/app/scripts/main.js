ec6c48ea0fbfd8d70275741afc642f26abe6cf1c
Ann <anngeran@gmail.com>
2020-07-22 20:01:13 +0300

    todos

--- todoList/app/scripts/main.js
+++ todoList/app/scripts/main.js
@@ -1,40 +1,57 @@
-var input = document.querySelector("input[type='text']");
-var ul = document.querySelector("ul");
-var container = document.querySelector("div");
-var lists = document.querySelectorAll("li");
-var spans = document.getElementsByTagName("span");
-var pencil = document.querySelector("#pencil");
-var saveBtn = document.querySelector(".save");
-var clearBtn = document.querySelector(".clear");
-var tipsBtn = document.querySelector(".tipBtn");
-var closeBtn = document.querySelector(".closebtn");
-var overlay = document.getElementById("overlay");
+import { keypress, ul_checked, pencil_show_hide, save_todo, clear_todo, tips, close } from "./clickers.js";
+import { cat_facts } from "./cat_facts.js";
 
+// Document ready
+$(document).ready(() => {
+  var spans = document.getElementsByTagName("span");
+  var ul = document.querySelector("ul");
+  var ul_cat_facts = document.querySelector('.cat-facts');
 
-//Delete todo If delete span clicked
+  // Initialize event listeners
+  keypress(deleteTodo);
+  ul_checked();
+  pencil_show_hide();
+  save_todo();
+  clear_todo();
+  tips();
+  close();
 
- function deleteTodo(){
-    for(let span of spans){
-        span.addEventListener('click', function(){
-            span.parentElement.remove();
-        })
+  // Load cat facts data
+  cat_facts("https://cat-fact.herokuapp.com/facts/random?animal_type&amount=10")
+    .done(data => {
+      console.log(data);
+      setFactsData(data);
+    })
+    .fail(err => {
+      console.log(err);
+    })
+
+  // Delete todo if delete span clicked
+  function deleteTodo() {
+    for (let span of spans) {
+      span.addEventListener("click", function () {
+        console.log(span.parentElement);
+        span.parentElement.remove();
+      });
     }
-}
+  }
 
-function loadTodos(){
-    if(localStorage.getItem("todoList")){
-        ul.innerHTML=localStorage.getItem("todoList");
-        deleteTodo();
+  function loadTodos() {
+    if (localStorage.getItem("todoList")) {
+      ul.innerHTML = localStorage.getItem("todoList");
     }
-}
+  }
 
+  function setFactsData(cat_facts) {
+    cat_facts.map(elem => {
+      ul_cat_facts.append(document.createElement('li'), elem.text)
+    })
+    console.log(ul_cat_facts);
+  }
 
-
-
-deleteTodo();
-
-loadTodos();
-
+  deleteTodo();
+  loadTodos();
+});
 
 
 
